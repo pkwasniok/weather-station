1f4fd206736a49eec5c6e1e5272a37a88737372d
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2025-09-08 15:21:24 +0200

    feat: temperature and pressure reading

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -8,6 +8,8 @@
 #include "esp_event.h"
 #include "esp_netif.h"
 #include "esp_wifi.h"
+#include "esp_err.h"
+#include "driver/i2c_master.h"
 
 #include "config.h"
 #include "mqtt.h"
@@ -17,18 +19,62 @@
 
 #define TAG "main"
 
+#define GPIO_I2C_SCL 3
+#define GPIO_I2C_SDA 5
+
 TaskHandle_t task_mqtt;
 
 int nvs_setup(void);
 int netif_setup(void);
 
 void app_main(void) {
-    assert(nvs_setup() == 0);
-    assert(netif_setup() == 0);
-    assert(wifi_setup() == 0);
-    assert(mqtt_setup() == 0);
+    i2c_master_bus_handle_t i2c_bus;
+    bmp280_device_t bmp280;
+
+    i2c_master_bus_config_t i2c_config = {
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .i2c_port = -1,
+        .scl_io_num = GPIO_I2C_SCL,
+        .sda_io_num = GPIO_I2C_SDA,
+        .flags.enable_internal_pullup = true,
+    };
+
+    bmp280_config_t bmp280_config = {
+        .standby_time = BMP280_STANDBY_4000_MS,
+        .temperature_oversampling = BMP280_OVERSAMPLING_X1,
+        .pressure_oversampling = BMP280_OVERSAMPLING_X4,
+        .filter = BMP280_FILTER_OFF,
+    };
+
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_config, &i2c_bus));
+    ESP_ERROR_CHECK(bmp280_init(&bmp280, &bmp280_config, i2c_bus));
+
+    ESP_ERROR_CHECK(bmp280_set_power_mode(&bmp280, BMP280_POWER_MODE_NORMAL));
+
+    ESP_ERROR_CHECK(nvs_setup());
+    ESP_ERROR_CHECK(netif_setup());
+    ESP_ERROR_CHECK(wifi_setup());
+    ESP_ERROR_CHECK(mqtt_setup());
 
     xTaskCreate(mqtt_task, "mqtt", 2048, NULL, 10, &task_mqtt);
+
+    while (1) {
+        int32_t temperature;
+        bmp280_get_temperature_degC_x100_int(&bmp280, &temperature);
+
+        uint32_t pressure;
+        bmp280_get_pressure_Pa_x1_int(&bmp280, &pressure);
+
+        char buffer[128];
+
+        sprintf(buffer, "%.2f", temperature / 100.0);
+        mqtt_publish("weather/temperature", buffer);
+
+        sprintf(buffer, "%.2f", pressure / 100.0);
+        mqtt_publish("weather/pressure", buffer);
+
+        vTaskDelay((1000 * 30) / portTICK_PERIOD_MS);
+    }
 }
 
 int nvs_setup(void) {
--- firmware/main/mqtt.h
+++ firmware/main/mqtt.h
@@ -3,4 +3,3 @@
 int mqtt_setup(void);
 void mqtt_task(void*);
 void mqtt_publish(char* topic, char* data);
-
